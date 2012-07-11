cb7ba3d73eddde9a88bfc80c96aeabab85bf0875
ingyesid <lazaro.yesid@gmail.com>
2012-07-11 14:18:29 -0500

    ic_launcher para todas las resolcuiones

--- Barcamp/src/com/orleonsoft/android/barcamp/HomeActivity.java
+++ Barcamp/src/com/orleonsoft/android/barcamp/HomeActivity.java
@@ -9,12 +9,13 @@ import android.os.Bundle;
 
 
 
+
 public class HomeActivity extends Activity {
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {		
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
+		setContentView(R.layout.home_screen);
 	}
 
 }
