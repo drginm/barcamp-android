facff8397a06767e03e417de371a361b3c464159
ingyesid <lazaro.yesid@gmail.com>
2012-07-18 21:22:32 -0500

    cambio en TwitterFeedFragment.java ahora solo baja una sola veez los tuits , mejoro fluides

--- Barcamp/src/com/orleonsoft/android/barcamp/TwitterFeedFragment.java
+++ Barcamp/src/com/orleonsoft/android/barcamp/TwitterFeedFragment.java
@@ -11,6 +11,7 @@ import com.orleonsoft.android.barcamp.network.TweetMessage;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,70 +20,79 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 /**
- *Archivo: TwitterFeedActivity.java
- *Autor:Yesid Lazaro lazaro.yesid@gmail.com / https://twitter.com/ingyesid
- *Fecha:12/07/2012
+ * Archivo: TwitterFeedActivity.java Autor:Yesid Lazaro lazaro.yesid@gmail.com /
+ * https://twitter.com/ingyesid Fecha:12/07/2012
  */
 
 public class TwitterFeedFragment extends Fragment {
-	
+
 	private View viewRoot;
 	private ListView listTimeLime;
-    private TextView labTwitterAccount;
+	private TextView labTwitterAccount;
+
+	private AdapterListTweets adapterListTweets;
 	
-	AdapterListTweets adapterListTweets;
-	
-	
-	
+	ArrayList<TweetMessage> timeLine;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
+		if (!Utils.isNetworkAvailable(getActivity().getApplicationContext())) {
+			Toast.makeText(getActivity().getApplicationContext(),
+					"Error cargando Tuits, no hay conexion a internet",
+					Toast.LENGTH_SHORT).show();
+
+		} else {
+			if (timeLine==null) {
+					timeLine=getTimeLine();
+					Log.d(AppsConstants.LOG_TAG, "get time line");
+			}
+		}
 		
-		
+		Log.d(AppsConstants.LOG_TAG, "OnCreate Fragment");
 	}
-	
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		viewRoot = inflater.inflate(R.layout.twitter_feed_screen, null); 
-		
-		listTimeLime=(ListView)viewRoot.findViewById(R.id.list_time_line);
-		labTwitterAccount=(TextView)viewRoot.findViewById(R.id.lab_twitter_account);
+		viewRoot = inflater.inflate(R.layout.twitter_feed_screen, null);
+
+		listTimeLime = (ListView) viewRoot.findViewById(R.id.list_time_line);
+		labTwitterAccount = (TextView) viewRoot
+				.findViewById(R.id.lab_twitter_account);
 		labTwitterAccount.setText(AppsConstants.TWITTER_ACCOUNT);
-		
-		if(!Utils.isNetworkAvailable(getActivity().getApplicationContext())){
-			Toast.makeText(getActivity().getApplicationContext(), "Error cargando Tuits, no hay conexion a internet", Toast.LENGTH_SHORT).show();
-						
-		}
-		else{
-			adapterListTweets= new AdapterListTweets(getActivity().getBaseContext(), android.R.layout.simple_list_item_1, getTimeLine());
-			listTimeLime.setAdapter(adapterListTweets);
-		}
-		
-		
-		return viewRoot; 
+
+		adapterListTweets = new AdapterListTweets(getActivity()
+				.getBaseContext(), android.R.layout.simple_list_item_1,
+				timeLine);
+		listTimeLime.setAdapter(adapterListTweets);
+
+		return viewRoot;
 	}
-	
-	public ArrayList<TweetMessage> getTimeLine(){
-		ArrayList<TweetMessage> result=new ArrayList<TweetMessage>();
-		 JSONObject object;
+
+	public ArrayList<TweetMessage> getTimeLine() {
+		ArrayList<TweetMessage> result = new ArrayList<TweetMessage>();
+		JSONObject object;
 		try {
-			object = JSONParser.getTweets(AppsConstants.RSS_TO_JSON_SERVICE_URL+AppsConstants.TWITTER_FEED_BARCAMP);
-						
-			JSONArray arrayTweets= object.getJSONObject("responseData").getJSONObject("feed").getJSONArray("entries");
-			
+			object = JSONParser.getTweets(AppsConstants.RSS_TO_JSON_SERVICE_URL
+					+ AppsConstants.TWITTER_FEED_BARCAMP);
+
+			JSONArray arrayTweets = object.getJSONObject("responseData")
+					.getJSONObject("feed").getJSONArray("entries");
+
 			for (int i = 0; i < arrayTweets.length(); i++) {
-				JSONObject auxObject=arrayTweets.getJSONObject(i);
-				result.add(new TweetMessage(auxObject.getString("content"), auxObject.getString("publishedDate")));
+				JSONObject auxObject = arrayTweets.getJSONObject(i);
+				result.add(new TweetMessage(auxObject.getString("content"),
+						auxObject.getString("publishedDate")));
 			}
 		} catch (JSONException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		return result;
-		
+
 	}
 
 }
